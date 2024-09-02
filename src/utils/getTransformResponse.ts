14a8300804ed57fefbcc578ae5968677ef37a79f
姚翰林 <yaohlin@yonyou.com>
2024-09-02 18:26:22 +0800

    修改bug

--- src/utils/getTransformResponse.ts
+++ src/utils/getTransformResponse.ts
@@ -5,19 +5,19 @@ import { loadModules } from 'yhl-utils'
 import { AxiosRequestConfig, Params } from '..'
 
 export default async function getTransformResponse(params: Params, axiosConfig: AxiosRequestConfig) {
-  const transformResponse = axiosConfig.transformResponse
-    ? Array.isArray(axiosConfig.transformResponse)
-      ? axiosConfig.transformResponse
-      : [axiosConfig.transformResponse]
-    : []
-
   if (params._Number2String) {
+    const transformResponse = axiosConfig.transformResponse
+      ? Array.isArray(axiosConfig.transformResponse)
+        ? axiosConfig.transformResponse
+        : [axiosConfig.transformResponse]
+      : []
+
     const JSONParse = await loadModules<typeof JSONParseType>(() => import('yhl-utils/es/JSONParse/JSONParse'))
     await JSONParse.init()
     transformResponse.unshift(function (data) {
       return JSONParse(data) || data
     })
-  }
 
-  return transformResponse
+    return transformResponse
+  }
 }
